22f11f715862268806b64d4f1ea07c4d2d28ddf8
khelf <khelf_mohamed@yahoo.fr>
2022-10-21 00:15:58 +0100

    pages: conferences-exposition-blog-contact

--- js/main.js
+++ js/main.js
@@ -1,119 +1,102 @@
 (function ($) {
-	"use strict";
+  "use strict";
 
-// TOP Menu Sticky
-$(window).on('scroll', function () {
-  var scroll = $(window).scrollTop();
-  var wScroll = $(this).scrollTop();
-  if (scroll > 100 && $(window).width() > 991) {
-    $("#navbar").removeClass("bg-transparent").css("background-color" , "#020d1f");
-  } else {
-    $("#navbar").addClass("bg-transparent") 
-  }
-	if (scroll < 400) {
-    $('#back-top').fadeIn(500);
-	} else {
-    $('#back-top').fadeIn(500);
-  }
-  		// Back To Top Appear
-      wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
-});
-
-if ($(window).width() < 992) {
-  $("#logo").css("width", "60px")
-}
-
-$(document).ready(function(){
-	
-		$('#back-to-top').on('click', function(){
-		$('body,html').animate({
-			scrollTop: 0
-		}, 600);
+  // TOP Menu Sticky
+  $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop();
+    var wScroll = $(this).scrollTop();
+    if (scroll > 100 && $(window).width() > 991) {
+      $("#navbar").removeClass("bg-transparent").css("background-color", "#020d1f");
+    } else {
+      $("#navbar").addClass("bg-transparent")
+    }
+    if (scroll < 400) {
+      $('#back-top').fadeIn(500);
+    } else {
+      $('#back-top').fadeIn(500);
+    }
+    // Back To Top Appear
+    wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
   });
 
-  // a script to toggle two button to show text in accueil
-			$("#btn1").attr("autofocus", true);
-			$("#btn1").addClass("btn-active");
-            $("#btn2").click(function () {
-                $("#btn1").attr("autofocus", false);
-				$("#btn1").removeClass("btn-active");
-                $("#btn2").attr("autofocus", true);
-                $("#matin").hide();
-                $("#midi").removeClass("d-none");
-            });
-            $("#btn1").click(function () {
-                $("#btn2").attr("autofocus", false);
-                $("#matin").show();
-                $("#midi").addClass("d-none");
-            });
-});
+  if ($(window).width() < 992) {
+    $("#logo").css("width", "60px")
+  }
 
-            $(".navbar-toggler").click(function() {
-                $('body').toggleClass('modal-open');
-            });
+  $(document).ready(function () {
+
+    $('#back-to-top').on('click', function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 600);
+    });
+  });
+
+  $(".navbar-toggler").click(function () {
+    $('body').toggleClass('modal-open');
+  });
 })(jQuery);
 
 /* Newsletter */
-function subscribetonewsletter(){
+function subscribetonewsletter() {
   var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange=function() {
-      console.log(this.readyState);
-         if (this.readyState == 4) {           
-        if(!(this.response).includes("Error")){
-    document.getElementById("newsletter_email").value ="";
-    document.getElementById("_subscribetonewsletterstatus").innerHTML="Merci de vous êtes inscrit à notre newsletter.";
-    }else {document.getElementById("_subscribetonewsletterstatus").innerHTML="Veuillez vérifier vos informations";}
-     }
-    };
-  xhttp.timeout = 5000;
-  xhttp.ontimeout=function (){
-      document.getElementById("_subscribetonewsletterstatus").innerHTML="Veuillez réessayer ou vérfier votre connexion";
+  xhttp.onreadystatechange = function () {
+    console.log(this.readyState);
+    if (this.readyState == 4) {
+      if (!(this.response).includes("Error")) {
+        document.getElementById("newsletter_email").value = "";
+        document.getElementById("_subscribetonewsletterstatus").innerHTML = "Merci de vous êtes inscrit à notre newsletter.";
+      } else { document.getElementById("_subscribetonewsletterstatus").innerHTML = "Veuillez vérifier vos informations"; }
+    }
   };
-  document.getElementById("_subscribetonewsletterstatus").innerHTML ="Envoi en cours...";
-  var email=document.getElementById("newsletter_email").value;
+  xhttp.timeout = 5000;
+  xhttp.ontimeout = function () {
+    document.getElementById("_subscribetonewsletterstatus").innerHTML = "Veuillez réessayer ou vérfier votre connexion";
+  };
+  document.getElementById("_subscribetonewsletterstatus").innerHTML = "Envoi en cours...";
+  var email = document.getElementById("newsletter_email").value;
   var state = false;
   state = validateEmail(email);
-  if(state){
-  var url="newsletter.php?&email="+encodeURIComponent(email);
-  xhttp.open("GET", url, true);
-  xhttp.send();
-  }else{
-  document.getElementById("_subscribetonewsletterstatus").innerHTML="Veuillez vérifier vos informations";
+  if (state) {
+    var url = "newsletter.php?&email=" + encodeURIComponent(email);
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  } else {
+    document.getElementById("_subscribetonewsletterstatus").innerHTML = "Veuillez vérifier vos informations";
   }
 
 }
 /* Devenir exposant */
-function devenirexposant(){
+function devenirexposant() {
   var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange=function() {
-      console.log(this.readyState);
-         if (this.readyState == 4) {           
-        if(!(this.response).includes("Error")){
-    document.getElementById("exposant_email").value ="";
-    document.getElementById("_devenirexposantstatus").innerHTML="Merci de vous êtes inscrit pour devenir exposant.";
-    }else {document.getElementById("_devenirexposantstatus").innerHTML="Veuillez vérifier vos informations";}
-     }
-    };
-  xhttp.timeout = 5000;
-  xhttp.ontimeout=function (){
-      document.getElementById("_devenirexposantstatus").innerHTML="Veuillez réessayer ou vérfier votre connexion";
+  xhttp.onreadystatechange = function () {
+    console.log(this.readyState);
+    if (this.readyState == 4) {
+      if (!(this.response).includes("Error")) {
+        document.getElementById("exposant_email").value = "";
+        document.getElementById("_devenirexposantstatus").innerHTML = "Merci de vous êtes inscrit pour devenir exposant.";
+      } else { document.getElementById("_devenirexposantstatus").innerHTML = "Veuillez vérifier vos informations"; }
+    }
   };
-  document.getElementById("_devenirexposantstatus").innerHTML ="Envoi en cours...";
-  var email=document.getElementById("exposant_email").value;
+  xhttp.timeout = 5000;
+  xhttp.ontimeout = function () {
+    document.getElementById("_devenirexposantstatus").innerHTML = "Veuillez réessayer ou vérfier votre connexion";
+  };
+  document.getElementById("_devenirexposantstatus").innerHTML = "Envoi en cours...";
+  var email = document.getElementById("exposant_email").value;
   var state = false;
   state = validateEmail(email);
-  if(state){
-  var url="exposant.php?&email="+encodeURIComponent(email);
-  xhttp.open("GET", url, true);
-  xhttp.send();
-  }else{
-  document.getElementById("_devenirexposantstatus").innerHTML="Veuillez vérifier vos informations";
+  if (state) {
+    var url = "exposant.php?&email=" + encodeURIComponent(email);
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  } else {
+    document.getElementById("_devenirexposantstatus").innerHTML = "Veuillez vérifier vos informations";
   }
 
 }
 
 function validateEmail(email) {
-var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-return re.test(String(email).toLowerCase());
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
-
